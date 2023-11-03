8d74434206a99804ead86530973688a3d57cea64
onjsdnjs <onjsdnjs@gmail.com>
2023-11-03 18:16:55 +0900

    lecture

--- src/main/java/io/concurrency/chapter01/exam03/CPUBoundExample.java
+++ src/main/java/io/concurrency/chapter01/exam03/CPUBoundExample.java
@@ -18,15 +18,20 @@ public class CPUBoundExample {
 
         for (int i = 0; i < numThreads; i++) {
             Future<?> future = executorService.submit(() -> {
+
+                // Cpu 연산이 집중되고 오래 걸리는 작업
                 long result = 0;
                 for (long j = 0; j < 1000000000L; j++) {
                     result += j;
                 }
+
+                // 아주 잠깐 대기함
                 try {
-                    Thread.sleep(10);
+                    Thread.sleep(1);
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
+
                 System.out.println("스레드: " + Thread.currentThread().getName() + ", " + result); // Cpu Bound 일때 ContextSwitching 은 크게 발생하지 않는다
             });
             futures.add(future);
--- src/main/java/io/concurrency/chapter01/exam03/IOBoundExample.java
+++ src/main/java/io/concurrency/chapter01/exam03/IOBoundExample.java
@@ -8,16 +8,25 @@ import java.util.concurrent.Executors;
 
 public class IOBoundExample {
     public static void main(String[] args) {
-        int numThreads = 3;
-        ExecutorService executorService = Executors.newFixedThreadPool(3);
+        int numThreads = Runtime.getRuntime().availableProcessors() / 2;
+        ExecutorService executorService = Executors.newFixedThreadPool(numThreads);
 
         for (int i = 0; i < numThreads; i++) {
             executorService.submit(() -> {
                 try {
+
+                    // IO 가 집중 되는 작업
                     for (int j = 0; j < 5; j++) {
-                        Files.readAllLines(Path.of("/Users/jeongsuwon/projects/ReactiveProgramming/src/main/java/io/reactive/reactiveprogramming/chapter01/exam04/sample.txt"));
+                        Files.readAllLines(Path.of("D:\\lecture\\Java-Concurrency-Programming\\Java-Concurrency-Programming\\src\\chapter01\\exam03\\sample.txt"));
                         System.out.println("스레드: " + Thread.currentThread().getName() +", " +j); // IO Bound 일때 ContextSwitching 이 일어난다
                     }
+
+                    // 아주 빠른 Cpu 연산
+                    int result = 0;
+                    for (long j = 0; j < 10; j++) {
+                        result += j;
+                    }
+
                 } catch (IOException e) {
                     e.printStackTrace();
                 }

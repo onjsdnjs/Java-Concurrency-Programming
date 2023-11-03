75a0e57eb5b2fb376989c48d0b874bbf280204ba
onjsdnjs <onjsdnjs@gmail.com>
2023-11-03 18:07:44 +0900

    lecture

--- src/main/java/io/concurrency/chapter01/exam03/CPUBoundExample.java
+++ src/main/java/io/concurrency/chapter01/exam03/CPUBoundExample.java
@@ -1,23 +1,47 @@
 package io.concurrency.chapter01.exam03;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class CPUBoundExample {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         int numThreads = Runtime.getRuntime().availableProcessors();
         ExecutorService executorService = Executors.newFixedThreadPool(numThreads);
 
+        long startTime2 = System.currentTimeMillis();
+        List<Future<?>> futures = new ArrayList<>();
+
         for (int i = 0; i < numThreads; i++) {
-            executorService.submit(() -> {
+            Future<?> future = executorService.submit(() -> {
                 long result = 0;
                 for (long j = 0; j < 1000000000L; j++) {
                     result += j;
                 }
-                System.out.println("스레드: " + Thread.currentThread().getName() +", " +result); // Cpu Bound 일때 ContextSwitching 은 크게 발생하지 않는다
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println("스레드: " + Thread.currentThread().getName() + ", " + result); // Cpu Bound 일때 ContextSwitching 은 크게 발생하지 않는다
             });
+            futures.add(future);
         }
+        futures.forEach(f -> {
+            try {
+                f.get();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            } catch (ExecutionException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        long endTime2 = System.currentTimeMillis();
+        System.out.println("CPU 개수를 초과하는 데이터를 병렬로 처리하는 데 걸린 시간: " + (endTime2 - startTime2) + "ms");
         executorService.shutdown();
     }
 }

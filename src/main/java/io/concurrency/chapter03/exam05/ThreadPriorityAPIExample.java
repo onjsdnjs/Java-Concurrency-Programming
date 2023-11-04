2ecfed4ae22c1d3615cad34c7056cf5ef38a5ffb
onjsdnjs <onjsdnjs@gmail.com>
2023-11-04 21:17:01 +0900

    lecture

--- src/main/java/io/concurrency/chapter03/exam05/ThreadPriorityAPIExample.java
+++ src/main/java/io/concurrency/chapter03/exam05/ThreadPriorityAPIExample.java
@@ -3,38 +3,28 @@ package io.concurrency.chapter03.exam05;
 public class ThreadPriorityAPIExample {
     public static void main(String[] args) {
 
-        //기본 우선순위 확인
         Thread thread = new Thread();
-        System.out.println("기본 우선순위: " + thread.getPriority());
-
-        //최대, 최소, 기본 우선순위 확인
-        System.out.println("최소 우선순위: " + Thread.MIN_PRIORITY);
-        System.out.println("기본 우선순위: " + Thread.NORM_PRIORITY);
-        System.out.println("최대 우선순위: " + Thread.MAX_PRIORITY);
-
-
-        //스레드 우선순위 변경하기
-        thread.setPriority(7);
-        System.out.println("우선순위 변경 후: " + thread.getPriority());
+        System.out.println("기본우선순위: "  + thread.getPriority());
         thread.start();
 
-        Thread minPriorityThread = new Thread(() -> {
-            System.out.println("최소 우선순위 스레드 실행, " + Thread.currentThread().getPriority());
-        });
-        minPriorityThread.setPriority(Thread.MIN_PRIORITY);
 
-        Thread normPriorityThread = new Thread(() -> {
-            System.out.println("보통 우선순위 스레드 실행, " + Thread.currentThread().getPriority());
+        Thread minThread = new Thread(() -> {
+            System.out.println("최소 우선순위 :" + Thread.currentThread().getPriority());
         });
-        normPriorityThread.setPriority(Thread.NORM_PRIORITY);
+        minThread.setPriority(Thread.MIN_PRIORITY);
+        minThread.start();
 
-        Thread maxPriorityThread = new Thread(() -> {
-            System.out.println("최대 우선순위 스레드 실행, " + Thread.currentThread().getPriority());
+        Thread normThread = new Thread(() -> {
+            System.out.println("기본 우선순위 :" + Thread.currentThread().getPriority());
         });
-        maxPriorityThread.setPriority(Thread.MAX_PRIORITY);
+        normThread.setPriority(Thread.NORM_PRIORITY);
+        normThread.start();
 
-        minPriorityThread.start();
-        normPriorityThread.start();
-        maxPriorityThread.start();
+        Thread maxThread = new Thread(() -> {
+            System.out.println("최대 우선순위 :" + Thread.currentThread().getPriority());
+        });
+        maxThread.setPriority(Thread.MAX_PRIORITY);
+        maxThread.start();
+
     }
 }
--- src/main/java/io/concurrency/chapter03/exam05/ThreadPriorityExample.java
+++ src/main/java/io/concurrency/chapter03/exam05/ThreadPriorityExample.java
@@ -3,37 +3,38 @@ package io.concurrency.chapter03.exam05;
 public class ThreadPriorityExample {
 
     public static void main(String[] args) throws InterruptedException {
-        Thread thread1 = new Thread(() -> {
-            for (int i = 0; i < 25; i++) {
-                System.out.println("우선순위 1의 스레드 실행");
-            }
-        });
-        thread1.setPriority(Thread.MIN_PRIORITY);
 
-        Thread thread3 = new Thread(() -> {
-            for (int i = 0; i < 25; i++) {
-                System.out.println("우선순위 3의 스레드 실행");
-            }
-        });
-        thread3.setPriority(3);
+        CountingThread maxThread = new CountingThread("우선 순위가 높은 스레드", Thread.MAX_PRIORITY);
+        CountingThread normThread = new CountingThread("우선 순위가 기본인 스레드", Thread.NORM_PRIORITY);
+        CountingThread minThread = new CountingThread("우선 순위가 낮은 스레드", Thread.MIN_PRIORITY);
 
-        Thread thread7 = new Thread(() -> {
-            for (int i = 0; i < 25; i++) {
-                System.out.println("우선순위 7의 스레드 실행");
-            }
-        });
-        thread7.setPriority(7);
+        maxThread.start();
+        normThread.start();
+        minThread.start();
 
-        Thread thread10 = new Thread(() -> {
-            for (int i = 0; i < 25; i++) {
-                System.out.println("우선순위 10의 스레드 실행");
-            }
-        });
-        thread10.setPriority(Thread.MAX_PRIORITY);
+        maxThread.join();
+        normThread.join();
+        minThread.join();
 
-        thread1.start();
-        thread3.start();
-        thread7.start();
-        thread10.start();
+        System.out.println("작업 완료");
+
+    }
+
+    static class CountingThread extends Thread {
+        private final String threadName;
+        private int count = 0;
+
+        public CountingThread(String threadName, int priority) {
+            this.threadName = threadName;
+            setPriority(priority);
+        }
+
+        @Override
+        public void run() {
+            while (count < 10000000) {
+                count++;
+            }
+            System.out.println(threadName + ": " + count);
+        }
     }
 }

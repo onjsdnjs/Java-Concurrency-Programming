eeae9cafa6f32b07ac274018ea0ef390ddb8deb1
onjsdnjs <onjsdnjs@gmail.com>
2023-11-06 20:25:38 +0900

    lecture

--- src/main/java/io/concurrency/chapter06/exam02/CountingSemaphoreExample.java
+++ src/main/java/io/concurrency/chapter06/exam02/CountingSemaphoreExample.java
@@ -25,27 +25,5 @@ public class CountingSemaphoreExample {
 
         System.out.println("최종 값: " + resource.getValue());
     }
-
-    static class SharedResource {
-        private int value = 0;
-        private CommonSemaphore commonSemaphore;
-
-        public SharedResource(CommonSemaphore commonSemaphore) {
-            this.commonSemaphore = commonSemaphore;
-        }
-
-        public void increment() {
-            commonSemaphore.acquired();
-            synchronized (this) {  // 카운팅 세마포어는 여러개의 스레드가 동시에 접근이 가능하므로 value에 대한 동기화를 해 주어야 한다
-                for (int i = 0; i < 1000000; i++) {
-                    value++;
-                }
-            }
-            commonSemaphore.release();
-        }
-        public int getValue() {
-            return value;
-        }
-    }
 }
 

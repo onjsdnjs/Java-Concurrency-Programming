d249d7cf5d523dbd79732909b66655057a789a40
onjsdnjs <onjsdnjs@gmail.com>
2023-11-06 17:21:10 +0900

    lecture

--- src/main/java/io/concurrency/chapter05/exam04/ThreadSafeMemberReferenceObjectExample.java
+++ src/main/java/io/concurrency/chapter05/exam04/ThreadSafeMemberReferenceObjectExample.java
@@ -1,9 +1,12 @@
 package io.concurrency.chapter05.exam04;
 
 public class ThreadSafeMemberReferenceObjectExample {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         new Thread(new MyRunnable(new Company("Company"))).start(); // 스레드에 안전함, 멤버변수를 공유하지 않음
         new Thread(new MyRunnable(new Company("Company"))).start(); // 스레드에 안전함, 멤버변수를 공유하지 않음
+
+        Thread.sleep(1000);
+        System.out.println("============================================================");
 
         Company company = new Company("Company"); // 스레드에 안전하지 못함, 멤버변수를 공유함
         new Thread(new MyRunnable(company)).start();

7c69e9fefb0bc570830e7bb342f96a149342fed5
onjsdnjs <onjsdnjs@gmail.com>
2023-11-07 17:42:54 +0900

    lecture

--- src/main/java/io/concurrency/chapter07/exam01/block/InstanceBlockSynchronizedExamples.java
+++ src/main/java/io/concurrency/chapter07/exam01/block/InstanceBlockSynchronizedExamples.java
@@ -1,8 +1,7 @@
 package io.concurrency.chapter07.exam01.block;
 
 public class InstanceBlockSynchronizedExamples {
-    private int count1 = 0;
-    private int count2 = 0;
+    private int count = 0;
 
     // 별도의 객체를 생성하여 모니터로 사용
     private final Object lockObject = new Object();
@@ -11,16 +10,16 @@ public class InstanceBlockSynchronizedExamples {
     // 특정 객체의 블록에 synchronized 키워드를 사용하는 방법
     public void incrementBlockThis() {
         synchronized (this) { // this 가 모니터가 된다
-            count1++;
-            System.out.println(Thread.currentThread().getName() + " - 블록 동기화로 증가: " + count1);
+            count++;
+            System.out.println(Thread.currentThread().getName() + " - 블록 동기화로 증가: " + count);
         }
     }
 
     // 별도의 객체를 사용하여 동기화하는 방법
     public void incrementBlockKLockObject() {
         synchronized (lockObject) { // Object 가 모니터가 된다
-            count2++;
-            System.out.println(Thread.currentThread().getName() + " - 별도 객체 동기화로 증가: " + count2);
+            count++;
+            System.out.println(Thread.currentThread().getName() + " - 별도 객체 동기화로 증가: " + count);
         }
     }
 
@@ -61,8 +60,8 @@ public class InstanceBlockSynchronizedExamples {
             t2.join();
             t3.join();
             t4.join();
-            System.out.println("count: " + example.count1);
-            System.out.println("count: " + example.count2);
+            System.out.println("count: " + example.count);
+
 
         } catch (InterruptedException e) {
             e.printStackTrace();

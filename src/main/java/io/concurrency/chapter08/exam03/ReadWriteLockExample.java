27a4d56b937c21a99a75a7d09103d381e0b6bb28
onjsdnjs <onjsdnjs@gmail.com>
2023-11-08 19:55:41 +0900

    lecture

--- src/main/java/io/concurrency/chapter08/exam03/ReadWriteLockExample.java
+++ src/main/java/io/concurrency/chapter08/exam03/ReadWriteLockExample.java
@@ -5,43 +5,58 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class ReadWriteLockExample {
     public static void main(String[] args) {
-
         ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
         SharedData sharedData = new SharedData();
 
-        // 읽기 작업을 수행하는 스레드
-        Thread readerThread1 = new Thread(() -> {
+        Thread reader1 = new Thread(() -> {
             readWriteLock.readLock().lock();
             try {
-                System.out.println("리더 스레드 1이 데이터를 읽고 있습니다. 데이터: " + sharedData.getData());
-                Thread.sleep(1000); // 읽기 작업 수행
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+                System.out.println("읽기 스레드 1이 데이터를 읽고 있습니다. 데이터: " + sharedData.getData());
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
             } finally {
                 readWriteLock.readLock().unlock();
             }
         });
 
-        // 쓰기 작업을 수행하는 스레드
-        Thread writerThread = new Thread(() -> {
+        Thread reader2 = new Thread(() -> {
+            readWriteLock.readLock().lock();
+            try {
+                System.out.println("읽기 스레드 2가 데이터를 읽고 있습니다. 데이터: " + sharedData.getData());
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            } finally {
+                readWriteLock.readLock().unlock();
+            }
+        });
+
+        Thread writer = new Thread(() -> {
             readWriteLock.writeLock().lock();
             try {
-                System.out.println("라이터 스레드가 데이터를 쓰고 있습니다.");
-                sharedData.setData(42); // 데이터를 변경
-                Thread.sleep(2000); // 쓰기 작업 수행
-                System.out.println("라이터 스레드가 데이터를 변경했습니다. 데이터: " + sharedData.getData());
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+                System.out.println("쓰기 스레드가 데이터를 쓰고 있습니다");
+                sharedData.setData(40);
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println("쓰기 스레드가 데이터를 변경 했습니다. 데이터: " + sharedData.getData());
             } finally {
                 readWriteLock.writeLock().unlock();
             }
         });
 
-        readerThread1.start();
-        writerThread.start();
+        writer.start();
+        reader1.start();
+        reader2.start();
     }
-
-    static class SharedData {
+    static class SharedData{
         private int data = 0;
 
         public int getData() {
@@ -52,5 +67,5 @@ public class ReadWriteLockExample {
             this.data = data;
         }
     }
-}
 
+}

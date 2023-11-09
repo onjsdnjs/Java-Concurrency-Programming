2ecd68e5eedba36f1bd67d7d69a4bfe127175d5b
onjsdnjs <onjsdnjs@gmail.com>
2023-11-09 22:03:13 +0900

    lecture

--- src/main/java/io/concurrency/chapter10/exam03/CallableExample.java
+++ src/main/java/io/concurrency/chapter10/exam03/CallableExample.java
@@ -3,30 +3,30 @@ package io.concurrency.chapter10.exam03;
 import java.util.concurrent.*;
 
 public class CallableExample {
-    public static void main(String[] args) {
-        // 스레드 풀 생성
+    public static void main(String[] args)  {
+
         ExecutorService executorService = Executors.newFixedThreadPool(1);
 
-        // Callable 을 사용한 예제
-        Callable<Integer> callableTask = () -> {
-            System.out.println("Callable 작업 수행 중...");
+        Callable<Integer> callableTask = ()->{
+            System.out.println("Callable 작업 수행중..");
+            System.out.println("Callable 작업 완료");
+
             return 42;
         };
+
         Future<Integer> future = executorService.submit(callableTask);
-        // Callable 작업 결과 가져 오기
-        int result = 0;
+        int result;
         try {
             result = future.get();
 
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
-
         } catch (ExecutionException e) {
             throw new RuntimeException(e);
         }
-        System.out.println("Callable 작업 결과: " + result);
 
-        // 스레드 풀 종료
+        System.out.println("Callable 작업 결과 : " + result);
+
         executorService.shutdown();
     }
 }
--- src/main/java/io/concurrency/chapter10/exam03/RunnableExample.java
+++ src/main/java/io/concurrency/chapter10/exam03/RunnableExample.java
@@ -5,16 +5,16 @@ import java.util.concurrent.Executors;
 
 public class RunnableExample {
     public static void main(String[] args) throws Exception {
-        // 스레드 풀 생성
+
         ExecutorService executorService = Executors.newFixedThreadPool(1);
 
-        // Runnable 을 사용한 예제
-        Runnable runnableTask = () -> {
-            System.out.println("Runnable 작업 수행 중...");
+        Runnable runnableTask = () ->{
+            System.out.println("Runnable 작업 수행 중..");
+            System.out.println("Runnable 작업 완료");
         };
+
         executorService.execute(runnableTask);
 
-        // 스레드 풀 종료
         executorService.shutdown();
     }
 }

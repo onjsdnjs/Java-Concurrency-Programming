ddac1e91ad48e5d740d93d20bb96e090cf18cfbd
onjsdnjs <onjsdnjs@gmail.com>
2023-11-10 22:35:45 +0900

    lecture

--- src/main/java/io/concurrency/chapter10/exam09/ScheduleWithFixedDelayExample.java
+++ src/main/java/io/concurrency/chapter10/exam09/ScheduleWithFixedDelayExample.java
@@ -11,7 +11,7 @@ public class ScheduleWithFixedDelayExample {
 
         Runnable task = () -> {
             try {
-                Thread.sleep(3000);
+                Thread.sleep(1000);
                 System.out.println("thread: " + Thread.currentThread().getName());
             } catch (InterruptedException e) {
                 // 예외 처리
@@ -19,7 +19,7 @@ public class ScheduleWithFixedDelayExample {
         };
 
         // 처음 1 초가 지난 후 실행 되고 작업이 완료 되고 나서 지정된 주기 마다 계속 실행 된다
-        ScheduledFuture<?> future = scheduler.scheduleWithFixedDelay(task, 1, 3, TimeUnit.SECONDS);
+        ScheduledFuture<?> future = scheduler.scheduleWithFixedDelay(task, 1, 1, TimeUnit.SECONDS);
 
         try {
             Thread.sleep(100000);

c0d211657cc48d69708d0b9d09e59a366b0e02c8
onjsdnjs <onjsdnjs@gmail.com>
2023-11-10 21:27:20 +0900

    lecture

--- src/main/java/io/concurrency/chapter10/exam07/AwaitTerminationExample.java
+++ src/main/java/io/concurrency/chapter10/exam07/AwaitTerminationExample.java
@@ -2,35 +2,32 @@ package io.concurrency.chapter10.exam07;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.ThreadFactory;
 
 public class AwaitTerminationExample {
     public static void main(String[] args) throws InterruptedException {
-        // ExecutorService 생성 및 데몬 스레드로 설정
-        ExecutorService executorService = Executors.newFixedThreadPool(1, runnable -> {
-            Thread thread = new Thread(runnable);
-            thread.setDaemon(true);
-            return thread;
-        });
 
-        // 작업 제출
-        executorService.submit(() -> {
-            while (true) {
-                System.out.println("데몬 스레드 1 실행 중...");
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
-                }
+        ExecutorService executorService = Executors.newFixedThreadPool(2, new ThreadFactory() {
+            @Override
+            public Thread newThread(Runnable r) {
+                Thread thread = new Thread(r);
+                thread.setDaemon(true);
+                return thread;
             }
         });
 
-        // ExecutorService 종료 요청
+        executorService.submit(()->{
+            while(true){
+                System.out.println(Thread.currentThread().getName() + " : 데몬 스레드 실행 중...");
+                Thread.sleep(1000);
+            }
+        });
+
         executorService.shutdown();
 
-        // ExecutorService 종료를 대기
-        executorService.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
-
+//        executorService.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+        Thread.sleep(3000);
         System.out.println("메인 스레드 종료");
+
     }
 }
--- src/main/java/io/concurrency/chapter10/exam07/ShutDownExample.java
+++ src/main/java/io/concurrency/chapter10/exam07/ShutDownExample.java
@@ -6,14 +6,14 @@ import java.util.concurrent.TimeUnit;
 
 public class ShutDownExample {
     public static void main(String[] args) {
+
         ExecutorService executorService = Executors.newFixedThreadPool(2);
 
-        // 작업 제출
         for (int i = 0; i < 5; i++) {
-            executorService.submit(() -> {
+            executorService.submit(()->{
                 try {
-                    Thread.sleep(1000);
-                    System.out.println(Thread.currentThread().getName() + " : 작업 완료");
+                    Thread.sleep(10000);
+                    System.out.println(Thread.currentThread().getName() + ": 작업 종료");
                 } catch (InterruptedException e) {
                     Thread.currentThread().interrupt();
                     throw new RuntimeException("인터럽트 걸림");
@@ -22,28 +22,29 @@ public class ShutDownExample {
             });
         }
 
-        // 스레드 풀 종료 요청
         executorService.shutdown();
 
-        // 스레드 풀이 종료될 때까지 대기
         try {
-            if (!executorService.awaitTermination(2, TimeUnit.SECONDS)) {
-                // 시간 내에 종료 되지 않으면 강제로 종료
+            if(!executorService.awaitTermination(1, TimeUnit.SECONDS)){
                 executorService.shutdownNow();
                 System.out.println("스레드 풀 강제 종료 수행");
             }
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
+            throw new RuntimeException(e);
         }
 
-        // 스레드 풀의 상태 확인
-        if (executorService.isShutdown()) {
-            System.out.println("스레드 풀을 종료 여부: " + executorService.isShutdown());
+        if(executorService.isShutdown()){
+            System.out.println("스레드 풀 종료 여부: " + executorService.isShutdown());
         }
 
-        while(!executorService.isTerminated()) {
-            System.out.println("스레드 풀 종료 중");
+        if(executorService.isTerminated()){
+            System.out.println("스레드 풀 완전 종료 여부: " + executorService.isTerminated());
         }
-        System.out.println("모든 작업이 완료되고 스레드 풀이 종료됨");
+
+        while(!executorService.isTerminated()){
+            System.out.println("스레드 풀 종료 중..");
+        }
+        System.out.println("모든 작업이 종료되고 스레드 풀이 종료됨");
     }
 }

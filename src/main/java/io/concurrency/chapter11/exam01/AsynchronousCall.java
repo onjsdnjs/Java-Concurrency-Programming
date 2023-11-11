2e967eb1f01394f569822d0c4547964d19427ab0
onjsdnjs <onjsdnjs@gmail.com>
2023-11-11 21:21:09 +0900

    lecture

--- src/main/java/io/concurrency/chapter11/exam01/AsynchronousCall.java
+++ src/main/java/io/concurrency/chapter11/exam01/AsynchronousCall.java
@@ -14,7 +14,9 @@ public class AsynchronousCall {
                 try {
                     //비동기 실행
                     asyncCall2();
-                    System.out.println("asyncCall1 종료");
+                    System.out.println("기다리지 않고 계속 작업 진행합니다");
+                    asyncCall3();
+                    System.out.println("동기 작업을 기다립니다.");
                     Thread.sleep(1000); // 1초 대기
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
@@ -27,7 +29,7 @@ public class AsynchronousCall {
 
         new Thread(() -> {
             try {
-                System.out.println("asyncCall2 종료");
+                System.out.println("비동기 작업 합니다");
                 Thread.sleep(1000); // 1초 대기
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
@@ -35,6 +37,17 @@ public class AsynchronousCall {
 
 
         }).start();
+
+    }
+
+    private static void asyncCall3() {
+
+    try {
+        System.out.println("동기 작업 합니다");
+        Thread.sleep(1000); // 1초 대기
+    } catch (InterruptedException e) {
+        throw new RuntimeException(e);
+    }
 
     }
 }

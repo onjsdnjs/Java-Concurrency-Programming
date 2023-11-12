998eec979b2d67bdf5499953cbe3d7fe6be7a1c0
onjsdnjs <onjsdnjs@gmail.com>
2023-11-12 17:26:27 +0900

    lecture

--- src/main/java/io/concurrency/chapter11/exam03/ExecutorServiceExample.java
+++ src/main/java/io/concurrency/chapter11/exam03/ExecutorServiceExample.java
@@ -1,31 +1,30 @@
-package io.concurrency.chapter11.exam03;
+package chapter11.exam03;
 
 import java.util.concurrent.*;
 
-public class FutureExample {
+public class ExecutorServiceExample {
 
     public static void main(String[] args) throws InterruptedException, ExecutionException {
 
         ExecutorService executorService = Executors.newFixedThreadPool(2);
 
-        Callable<Integer> task1 = () -> 2;
-        Callable<Integer> task2 = () -> {
-            int result = task1.call() + 3;
+        Future<Integer> future1 = executorService.submit(() -> 2);
+        Future<Integer> future2 = executorService.submit(() -> {
+            int result = future1.get() + 3;
             return result;
-        };
+//            throw new RuntimeException("error"););
+        });
 
-        Future<Integer> future2 = executorService.submit(task2);
-
-        int intermediateResult;
+        int result;
         try {
-            intermediateResult = future2.get();
+            result = future2.get();
 
         } catch (InterruptedException | ExecutionException e) {
             System.out.println("예외 발생: " + e.getMessage());
-            intermediateResult = 0; // 예외가 발생하면 0을 반환
+            result = 0; // 예외가 발생하면 0을 반환
         }
 
-        int finalResult = intermediateResult;
+        int finalResult = result;
 
         System.out.println("최종 결과: " + finalResult);
         executorService.shutdown();

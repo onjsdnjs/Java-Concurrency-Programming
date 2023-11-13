bcc67ec0ae47dc42a63017b2153e7d36019a2a9b
onjsdnjs <onjsdnjs@gmail.com>
2023-11-13 17:28:42 +0900

    lecture

--- src/main/java/io/concurrency/chapter11/exam05/ThenApplyExample.java
+++ src/main/java/io/concurrency/chapter11/exam05/ThenApplyExample.java
@@ -6,13 +6,20 @@ public class ThenApplyExample {
     public static void main(String[] args) {
 
         MyService myService = new MyService();
-        CompletableFuture<Integer> asyncFuture = myService.fetchAsyncData()
+        CompletableFuture<Integer> asyncFuture = CompletableFuture.supplyAsync(() -> {
+                                                    try {
+                                                        Thread.sleep(1000);
+                                                    } catch (InterruptedException e) {
+                                                        Thread.currentThread().interrupt();
+                                                    }
+                                                    return 40;
+                                                })
                                                 .thenApply(result -> {
-                                                    int r = myService.performData2(result);
+                                                    int r = myService.getData1();
                                                     System.out.println("thread:" + Thread.currentThread().getName());
                                                     return r + result;
                                                 }).thenApplyAsync(result -> {
-                                                    int r = myService.performData2(result);
+                                                    int r = myService.getData2();
                                                     System.out.println("thread:" + Thread.currentThread().getName());
                                                     return r + result;
                                                 });
@@ -23,22 +30,22 @@ public class ThenApplyExample {
 
     static class MyService {
 
-        public CompletableFuture<Integer> fetchAsyncData() {
-            return CompletableFuture.supplyAsync(() -> {
-                try {
-                    Thread.sleep(1000); // 2초 동안 대기
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
-                }
-                return 40;
-            });
+        public int getData1(){
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            return 50;
         }
 
-        public int performData(int input) {
-            return input * 2;
-        }
-        public int performData2(int input) {
-            return input * 2;
+        public int getData2(){
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            return 60;
         }
     }
 }

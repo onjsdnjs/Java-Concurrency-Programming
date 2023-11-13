6350805650e245a639acf6b19f56092df401bb8a
onjsdnjs <onjsdnjs@gmail.com>
2023-11-13 18:24:32 +0900

    lecture

--- src/main/java/io/concurrency/chapter11/exam05/ThenApplyExample.java
+++ src/main/java/io/concurrency/chapter11/exam05/ThenApplyExample.java
@@ -6,29 +6,36 @@ public class ThenApplyExample {
     public static void main(String[] args) {
 
         MyService myService = new MyService();
+        long started = System.currentTimeMillis();
         CompletableFuture<Integer> asyncFuture = CompletableFuture.supplyAsync(() -> {
                                                     System.out.println("thread1:" + Thread.currentThread().getName());
+                                                    try {
+                                                        Thread.sleep(1000);
+                                                    } catch (InterruptedException e) {
+                                                        throw new RuntimeException(e);
+                                                    }
                                                     return 40;
                                                 })
                                                 .thenApply(result -> {
-                                                    int r = myService.getData1();
                                                     System.out.println("thread2:" + Thread.currentThread().getName());
+                                                    int r = myService.getData1();
                                                     return r + result;
-                                                }).thenApply(result -> {
-                                                    int r = myService.getData2();
+                                                }).thenApplyAsync(result -> {
                                                     System.out.println("thread3:" + Thread.currentThread().getName());
+                                                    int r = myService.getData2();
                                                     return r + result;
                                                 });
 
         int asyncResult = asyncFuture.join();
         System.out.println("final result: " + asyncResult);
+        System.out.println("총 소요 시간: " + (System.currentTimeMillis() - started));
     }
 
     static class MyService {
 
         public int getData1(){
             try {
-                Thread.sleep(500);
+                Thread.sleep(1000);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
@@ -37,7 +44,7 @@ public class ThenApplyExample {
 
         public int getData2(){
             try {
-                Thread.sleep(500);
+                Thread.sleep(1000);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
